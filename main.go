663de21bce435661132dffde684b838c5505c0fc
Edgar Lins <edgarlins.dev@gmail.com>
2024-08-09 16:10:43 -0300

    Class: methods

--- main.go
+++ main.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
+type GameState struct {
+	coins     int
+	life      int
+	blood     int
+	character string
+}
+
+func (g GameState) GetCharacterAndBlood() (string, int) {
+	return g.character, g.blood
+}
+
 func main() {
-	fmt.Println("Hello, world!")
+	state1 := GameState{
+		blood:     13,
+		character: "Mario",
+		coins:     122,
+		life:      2,
+	}
+
+	c, b := state1.GetCharacterAndBlood()
+	fmt.Printf("character: %s blood: %d \n", c, b)
+
+	state2 := GameState{
+		blood:     63,
+		character: "Princess",
+		coins:     122,
+		life:      4,
+	}
+
+	c2, b2 := state2.GetCharacterAndBlood()
+	fmt.Printf("character: %s blood: %d", c2, b2)
 }
